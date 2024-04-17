7eba2b9a759f2fe637eb1587aeda61b48a96c497
zacktagnan <zacktagnan@gmail.com>
2024-04-17 02:23:58 +0200

    Initial-commit-Entorno.testing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sobre tdd-todo_list
+
+Tratando de ir implementando un proyecto básico para la gestión de un listado de tareas aplicando la técnica del TDD (Test Driven Development).
+
+- Según se vayan recibiendo errores en los tests establecidos, se irá desarrollando lo necesario para pasar dichos tests.
+
+- Una vez todos los tests pasados relativos a todas las posibles funcionalidades de la aplicación, es cuando se podrá pasar a la refactorización del código. Siempre respetando lo establecido en los tests.
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    protected $usersDefault;
+
+    public function __construct()
+    {
+        $this->usersDefault = [
+            [
+                'name' => 'Admin Test',
+                'email' => 'admin@test.es',
+                'email_verified_at' => now(),
+                'password' => Hash::make('12345678'),
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'name' => 'User Test',
+                'email' => 'user@test.es',
+                'email_verified_at' => now(),
+                'password' => Hash::make('xxxxxxxx'),
+                'remember_token' => Str::random(10),
+            ],
+        ];
+    }
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Datos fijos...
+        foreach ($this->usersDefault as $row) {
+            User::create([
+                'name' => $row['name'],
+                'email' => $row['email'],
+                'email_verified_at' => $row['email_verified_at'],
+                'password' => $row['password'],
+                'remember_token' => $row['remember_token'],
+            ]);
+        }
+
+        // Datos de relleno ficticios...
+        // User::factory()->count(26)->create();
+        // o
+        // User::factory(26)->create();
+    }
+}
