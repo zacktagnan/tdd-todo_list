7610e5baeab950f10f98cf3d2ffab9b2c944256e
zacktagnan <zacktagnan@gmail.com>
2024-04-18 00:54:16 +0200

    Tabla.tasks-agregada.columna.description

--- database/migrations/2024_04_16_014918_create_tasks_table.php
+++ database/migrations/2024_04_16_014918_create_tasks_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->text('description');
             $table->boolean('completed')->default(false);
             $table->timestamps();
         });
