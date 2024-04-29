7de79d399929c424fcc8ff1faf6d6d2af4c75e86
zacktagnan <zacktagnan@gmail.com>
2024-04-29 13:51:44 +0200

    Cambios.claves.traducciones.en.Create.y.Edit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            {{ __('tasks/general.section_label') }}
+            {{ __('tasks/index.general_label') }}
         </h2>
     </x-slot>
 
